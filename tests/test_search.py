7d77f045ead4c38a05a453de42fec1e50ca9ac99
Poojita9185 <poojita9185@gmail.com>
2025-01-04 15:02:26 +0530

    Added few more allure reports

--- tests/test_search.py
+++ tests/test_search.py
@@ -7,11 +7,14 @@ from tests.test_basefile import BaseFile
 class TestSearch(BaseFile):
 
     @allure.description("Searching product")
+    @allure.id("id123")
     def test_search_product(self):
         homepage = HomePage(self.driver)
         homepage.go_to_home_page()
         homepage.enter_product_into_search("HP")
 
+    @allure.severity("High")
+    @allure.story("GSCE")
     def test_click_product(self):
         homepage = HomePage(self.driver)
         homepage.click_product()
